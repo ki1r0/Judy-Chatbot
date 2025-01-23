5d927c4b27cef3bf8d5cd82256184eacca7db5ae
ki1r0 <D1ckyTwist3r@users.noreply.github.com>
2025-01-24 02:33:21 +0800

    Echo

--- src/main/java/Judy.java
+++ src/main/java/Judy.java
@@ -1,16 +1,17 @@
-public class Duke {
+public class Judy {
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
-        String dashline = "____________________________________________________________\n";
+        String separator = "____________________________________________________________\n";
         String logo = "Judy";
-        System.out.println(dashline + " Hello! I'm " + logo + "\n" +
-                " What can I do for you? ");
+        System.out.println(separator + " Hello! I'm " + logo + "\n" +
+                " What can I do for you? \n" + separator);
 
         String message = inputHandler.getMessage();
-        if (message.equals("")) {
-            System.out.println(dashline + " Bye. Hope to see you again soon!");
+        while (!message.equals("bye")) {
+
+            System.out.println(message + "\n" + separator);
+            message = inputHandler.getMessage();
         }
-
-
+        System.out.println(" Bye. Hope to see you again soon! \n" + separator);
     }
 }
